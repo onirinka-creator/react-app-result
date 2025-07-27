3f35749c0593110501799fa402cfbc0b5b7e3168
Onirinka <onirinka.job@gmail.com>
2025-07-27 10:34:07 +0300

    Exc 2 (no jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-	// Императивный
-  const [count, setCount] = useState(0)
-	// Декларативный
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-		{/* Императивнй */}
-        <button onClick={() => setCount((count) => count + 1)}> {/* императивный */}
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-	  <p>
-		{/* декларативный */}
-		{new Date().getFullYear()}
-	  </p>
-    </>
-  )
+  const [count, setCount] = useState(0);
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "a",
+        {
+          href: "https://vite.dev",
+          target: "_blank",
+        },
+        React.createElement("img", {
+          src: viteLogo,
+          className: "logo",
+          alt: "Vite logo",
+        })
+      ),
+      React.createElement(
+        "a",
+        {
+          href: "https://react.dev",
+          target: "_blank",
+        },
+        React.createElement("img", {
+          src: reactLogo,
+          className: "logo",
+          alt: "React logo",
+        })
+      )
+    ),
+    React.createElement("h1", null, "Vite + React"),
+    React.createElement(
+      "div",
+      { className: "card" },
+      React.createElement(
+        "button",
+        {
+          onClick: () => setCount((count) => count + 1),
+        },
+        `count is ${count}`,
+      ),
+      React.createElement(
+        "p",
+        null,
+        `Edit `,
+        React.createElement("code", null, "src/App.jsx"),
+        ` and save to test HMR`
+      )
+    ),
+    React.createElement(
+      "p",
+      { className: "read-the-docs" },
+      "Click on the Vite and React logos to learn more"
+    )
+  );
 }
 
-export default App
+export default App;
